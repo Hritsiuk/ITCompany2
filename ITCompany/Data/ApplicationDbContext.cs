d1e21421cd4324724a9587109dc4038bd42ab664
Максим Левчук <levchukmaks2001@gmail.com>
2020-11-10 23:01:18 +0200

    Добавьте файлы проекта.

---
+++ ITCompany/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ITCompany.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITCompany.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
